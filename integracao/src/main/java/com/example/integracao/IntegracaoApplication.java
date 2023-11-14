6e8f382dccd6b6b8a12422600753b1a99e7551a7
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-11-14 13:41:39 -0300

    :bricks: backend java spring initial

---
+++ integracao/src/main/java/com/example/integracao/IntegracaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.integracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoApplication.class, args);
+	}
+
+}
---
+++ integracao/src/test/java/com/example/integracao/IntegracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
